83058197d027ad262f98f25cf72d988e7bf18812
unknown <alicia9790972@gmail.com>
2022-09-21 19:06:10 -0400

    added game start function and new vars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,94 @@
 //assign vars
-var timerEl = document.querySelector("countdown");
+var timerEl = document.getElementById("timer");
 var startButton = document.querySelector("#start-btn");
+var submitButton = document.querySelector("#sub-btn");
+var nameInput = document.querySelector("#user-input");
+
 var secondsLeft = 50;
 var score = 0;
-var nameInput = document.querySelector("#user-input");
-var nameS = nameInput.value
+var nameS = nameInput.value;
+questionNum = 0;
 
 //questions/answers set in an array
 var questions = [
     {
-        prompt: 'fdsfdg',
-        answer:'a'
+        prompt: 'Which would be the largest heading?(avhjb /n',
+        choices: ['<h1>','<h3>','<main>'],
+        answer:'<h1>'
     },
     {
-        prompt: 'DSFDSGFDSGFDS',
-        answer:'a'
+        prompt: 'What does HTML stand for?',
+        choices: ['Hypertext Madeup Language','Hypertext Markup Language','Hypo Makes Language'],
+        answer:'Hypertext Markup Language'
     },
     {
-        prompt: 'DSFDSGFDSGFDS',
-        answer:'a'
+        prompt: 'What does CSS stand for?',
+        choices: ['Cascading Style Sheets','Circular Style Sheets','Copper Styling Sheet'],
+        answer:'Cascading Style Sheets'
     },
     {
-        prompt: 'DSFDSGFDSGFDS',
-        answer:'a'
+        prompt: 'Where should the <script> code go in an HTML file?',
+        choices: ['header','end of body','div'],
+        answer:'end of body'
     },
     {
-        prompt: 'DSFDSGFDSGFDS',
-        answer:'a'
+        prompt: 'How can you make a list that lists the items with bullets? ',
+        choices: ['<ul>','<h3>','<span>'],
+        answer:'<ul>'
     }
 ];
 
 //starts program, starts timer and generates questions
-function startProgram() {
-    timerStart()
+function startProgram(event) {
+    event.preventDefault()
+    timerCount()
     generateQuestions()
-
-    for (var i=0; i < questions.length;i++) {
-        var response = window.prompt(questions[i].prompt);
-        if(response == questions[i].answer){
-             score++;
-             alert("Correct!");
-        } else {
-             alert("WRONG! - 5 seconds (:");
-             subtractSec();
-        }
+    startButton.style.visibility = 'hidden';
+    questionNum = 0;
+    timer = 90;
+    questionC.textContent = Object.values(questions[questionNum])[0];
+    for (i = 0; i < questions[questionNum].choices.length; i++) {
+        var choice = document.createElement("button");
+        choice.textContent = Object.values(questions[questionNum])[1][i];
+        questionC.appendChild(choice);
     }
-    alert("you got " + score + "/" + questions.length);
+
+ 
 };
 
 //timer starts
- function timerStart() {
-      timerEl.textContent = secondsLeft + " Seconds Left.";
-      if (timeLeft > 1) {
-        timerEl.textContent = timeLeft + ' seconds remaining'
-        secondsLeft--;
-    } else {
-        clearInterval(timerInterval);
-         sendMessage()
-       };
-     };
+var timerCount = setInterval (function() {
+    secondsLeft--;
+    timerEl.textContent = `Timer: ${secondsLeft}`
+    if (secondsLeft === 0 ){
+        endGame()
 
- function sendMessage() {
-    timerEl.textContent = "Times Up!";
-};
+    };
+}, 1000);
 
 // subtracting 5 seconds when an answer is incorect
 function subtractSec() {
     secondsLeft = secondsLeft -5 
 };
 
-//storing local storage for the highscore board
-localStorage.setItem('name',nameS)
+//showing questions
+// function generateQuestions() {
+//     if ()
+// }
+
+//confirming answer
+// function confirmAnswer() {
+//     if ()
+// }
 
 //button function
-startButton.addEventListener('click', function () {
-    alert('Hello World');
-  });
+
+//startgame button
+startButton.addEventListener('click ', startProgram);
+
+
+//HELP HERE
+//save initals button
+submitButton.addEventListener('click', function (event) {
+     localStorage.setItem('name',nameS);
+});
