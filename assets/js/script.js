c611402fae05e76a99ed3cb5963720ca925ef0ec
unknown <alicia9790972@gmail.com>
2022-09-21 20:09:10 -0400

    added checking feature and end function, not connecting to html tho?

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,15 @@ var timerEl = document.getElementById("timer");
 var startButton = document.querySelector("#start-btn");
 var submitButton = document.querySelector("#sub-btn");
 var nameInput = document.querySelector("#user-input");
+var questionChoice = document.getElementById("multiple-choice");
+var scoreB = document.getElementById("scorebox");
 
 var secondsLeft = 50;
 var score = 0;
 var nameS = nameInput.value;
-questionNum = 0;
+var questionNum = 0;
+
+scoreB.style.visibility = 'hidden';
 
 //questions/answers set in an array
 var questions = [
@@ -44,16 +48,86 @@ function startProgram(event) {
     timerCount()
     generateQuestions()
     startButton.style.visibility = 'hidden';
-    questionNum = 0;
-    timer = 90;
-    questionC.textContent = Object.values(questions[questionNum])[0];
+};
+
+//gets called by start program shows questions, 
+//if less than # of q's will move on to next question
+function generateQuestions() {
+    questionChoice.textContent = Object.values(questions[questionNum])[0];
     for (i = 0; i < questions[questionNum].choices.length; i++) {
         var choice = document.createElement("button");
         choice.textContent = Object.values(questions[questionNum])[1][i];
-        questionC.appendChild(choice);
+        questionChoice.appendChild(choice);
     }
+};
 
- 
+//checks response from question
+function checkAnswer(event, answer) {
+    var answer = event.target
+    var result = false;
+    //if right and more questions will move on to next q
+    if (questionNum < questions.length) {
+        var questionAnswer = Object.values(questions[questionNum])[2];
+    };
+    //if wrong and is less than # of qs will remove time and go next
+    if (answer.textContent !== questionAnswer) {
+        secondsLeft = secondsLeft -5
+        questionNum++;
+        if (questionNum === questions.length) {
+            endGame(score, timerEl, result);
+        } else {
+            nextQuestion(questionNum, result);
+        }
+    //ends game if all questions / max length reached
+    } else {
+        score++;
+        questionNum++;
+        result = true;
+        if (questionNum === questions.length) {
+            endGame(score, timerEl, result);
+        } else {
+            nextQuestion(questionNum, result);
+        }
+    };
+}
+
+//called by check is question # is less than the length will execute
+function nextQuestion(questionNum, result) {
+    if (questionNum < questions.length) {
+        questionC.textContent = Object.values(questions[questionNum])[0];
+        var resultText = document.createElement("p");
+        questionC.appendChild(resultText);
+        if (result) {
+            resultText.textContent = "Correct!";
+        } else {
+            resultText.textContent = "Wrong!";
+        };
+        for (i = 0; i < questions[questionNum].choices.length; i++) {
+            var choice = document.createElement("button");
+            choice.textContent = Object.values(questions[questionNum])[1][i];
+            questionC.appendChild(choice);
+        }
+    };
+}
+
+//called after all questions are complete
+function endGame(score, timer, result) {
+    var submitButton = document.querySelector("#sub-btn");
+    var nameInput = document.querySelector("#user-input");
+    var scoreSaver = document.createElement("input");
+    var finalScore = timer+score
+    var resultText = document.createElement("p");
+    scoreSaver.type = "text";
+    startButton.style.visibility = "visible";
+    questionChoice.textContent = "Game Over! Your score was! " + (finalScore) + " Enter your initials!: ";
+    questionChoice.appendChild(resultText);
+    clearInterval(timerCount);
+    questionC.appendChild(scoreSaver);
+    if (result) {
+        resultText.textContent = "Correct!";
+    } else {
+        resultText.textContent = "Wrong!";
+    };
 };
 
 //timer starts
@@ -62,30 +136,15 @@ var timerCount = setInterval (function() {
     timerEl.textContent = `Timer: ${secondsLeft}`
     if (secondsLeft === 0 ){
         endGame()
-
     };
 }, 1000);
 
-// subtracting 5 seconds when an answer is incorect
-function subtractSec() {
-    secondsLeft = secondsLeft -5 
-};
-
-//showing questions
-// function generateQuestions() {
-//     if ()
-// }
-
-//confirming answer
-// function confirmAnswer() {
-//     if ()
-// }
-
 //button function
-
 //startgame button
 startButton.addEventListener('click ', startProgram);
 
+//next question/check button
+questionC.addEventListener("click", checkAnswer);
 
 //HELP HERE
 //save initals button
